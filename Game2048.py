715c99cec11a6f6989d12b22169f6e84d9306c17
daniyaliqbal2024 <86933570+daniyaliqbal2024@users.noreply.github.com>
2023-06-21 23:18:38 -0500

    next commit

--- Game2048.py
+++ Game2048.py
@@ -77,6 +77,28 @@ class Game(tk.Frame):
         2048: ("Helvetica", 40, "bold"),
     }
 
+    def make_GUI (self): 
+        self.cells= []
+        for i in range(4):
+            row=[]
+            for j in range(4):
+                cell_frame= tk.Frame(
+                    self.main_grid,
+                    bg=self.Color_EmptyCell,
+                    width= 150,
+                    height=150
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number= tk.Label(self.main_grid, bg=self.Color_EmptyCell)
+                cell_number.grid(row=i, column=j)
+                cell_data= {"frame":cell_frame, "number":cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        score_frame=tk.Frame(self)
+        score_frame.place(relx=0.5, y=45, anchor="center")
+        
+
 
     
 
